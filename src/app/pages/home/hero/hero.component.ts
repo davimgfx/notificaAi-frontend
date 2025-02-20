9bafdf02a08c86675a2a9339ade452ba0401f264
Davi Fernandes <davifncosta@gmail.com>
2025-02-20 20:48:36 -0300

    feat: navbar responsive

--- src/app/pages/home/hero/hero.component.ts
+++ src/app/pages/home/hero/hero.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../shared/components/button/button.component';
+import { AUTH } from '../../../core/models/constants-routing';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [RouterLink],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
 })
 export class HeroComponent {
+    AUTH_ROUTER = AUTH;
 
 }
